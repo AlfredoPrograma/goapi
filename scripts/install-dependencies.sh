40802d1d5764afff01dd7c8a22c2a73bfe126d5b
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-02-24 01:14:39 +0000

    chores(templates): add htmx as dependency and link page template to file

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+ASSETS_FOLDER="./public/assets"
+
 # Install SQLC
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@v1.25.0
 
@@ -12,3 +14,6 @@ go install github.com/cosmtrek/air@v1.49.0
 
 # Install Templ
 go install github.com/a-h/templ/cmd/templ@v0.2.543
+
+# Install HTMX
+curl --output-dir $ASSETS_FOLDER -sO https://unpkg.com/htmx.org@1.9.10/dist/htmx.min.js 
